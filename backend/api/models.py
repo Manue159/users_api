0eda3781c58d218a0320fe81cb03ecffc142f239
Manue159 <manuegn145@gmail.com>
2024-11-22 15:32:12 +0100

    files added

---
+++ backend/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.firstname
+    
+    
